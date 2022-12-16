6096381ce93fe3e140d68aa7044bdd91fa1694dc
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2022-12-16 15:59:36 -0500

    add background and logic

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,64 @@
 var searchButton = document.getElementById("sbmitbttn");
 var cityInput = document.getElementById("inputSearch");
+var pastCity = document.getElementById("saved");
+
+function currentCardFormatter(weather) {
+  console.log("Current wether Formatter");
+  console.log(weather);
+  //where am I appending this information to?
+  var currentCardPlace = document.getElementById("display");
+  //DO I need to clear where I'm appending or do I want duplicates?
+  currentCardPlace.innerHTML = "";
+  // What element do I create, what will be its content, and where do I append it to?
+  currentCardPlace.innerHTML += `
+<div>
+  <p> ${weather.dt_txt} </p>
+  <p> Weather: ${weather.weather[0].main} </p>
+  <p> Description: ${weather.weather[0].description}</p>
+  <p> Temp: ${weather.main.temp}</p>
+  <p> 
+  
+</div>`;
+}
+
+function fiveDayFormatter(weatherArr) {
+  console.log("Five Days coming up!");
+  console.log(weatherArr);
+  var fiveDayCard = document.getElementById("five-day");
+  fiveDayCard.innerHTML = "";
+  fiveDayCard.innerHTML += `
+  <div class="col-2" >
+  <p> ${weatherArr[0].dt_txt} </P>
+  <p> Weather: ${weatherArr[0].weather[0].main} </p>
+  <p> Descr: ${weatherArr[0].weather[0].description}</p>
+  <p> Temp: ${weatherArr[0].main.temp}</p>
+  </div>
+  <div>
+  <p> ${weatherArr[1].dt_txt} </P>
+  <p> Weather: ${weatherArr[1].weather[0].main} </p>
+  <p> Description: ${weatherArr[1].weather[0].description}</p>
+  <p> Temp: ${weatherArr[1].main.temp}</p>
+  </div>
+  <div>
+  <p> ${weatherArr[2].dt_txt} </P>
+  <p> Weather: ${weatherArr[2].weather[0].main} </p>
+  <p> Description: ${weatherArr[2].weather[0].description}</p>
+  <p> Temp: ${weatherArr[2].main.temp}</p>
+  </div>
+  <div>
+  <p> ${weatherArr[3].dt_txt} </P>
+  <p> Weather: ${weatherArr[3].weather[0].main} </p>
+  <p> Description: ${weatherArr[3].weather[0].description}</p>
+  <p> Temp: ${weatherArr[3].main.temp}</p>
+  </div>
+  <div>
+  <p> ${weatherArr[4].dt_txt} </P>
+  <p> Weather: ${weatherArr[4].weather[0].main} </p>
+  <p> Description: ${weatherArr[4].weather[0].description}</p>
+  <p> Temp: ${weatherArr[4].main.temp}</p>
+  </div>
+  `;
+}
 
 function beginSearch(event) {
   event.preventDefault();
@@ -9,23 +68,25 @@ function beginSearch(event) {
   if (cityName) {
     findCity(cityName);
     console.log(cityName);
-    cityInput.value = "";
   } else {
     alert("enter a city name");
   }
-
-  saveCity();
 }
 function saveCity() {
-  var searched = {
-    city: cityInput.value.trim(),
-  };
+  var searched = cityInput.value.trim();
   var storedCity = JSON.parse(localStorage.getItem("found"));
   if (storedCity === null) {
     storedCity = [];
   }
+  if (storedCity.includes(searched)) {
+    return;
+  }
   storedCity.push(searched);
+  if (storedCity.length > 6) {
+    storedCity.shift();
+  }
   localStorage.setItem("found", JSON.stringify(storedCity));
+  init();
 }
 var findCity = function (cityId) {
   var url =
@@ -51,7 +112,8 @@ var findCity = function (cityId) {
         lat +
         "&lon=" +
         lon +
-        "&appid=1fe1270c934256178352d8b9b50ce7a4";
+        "&appid=1fe1270c934256178352d8b9b50ce7a4" +
+        "&units=imperial";
       console.log(newUrl);
       fetch(newUrl)
         .then(function (response) {
@@ -63,7 +125,15 @@ var findCity = function (cityId) {
           }
         })
         .then(function (weather) {
-          console.log(weather);
+          saveCity();
+          console.log(weather.list);
+          currentCardFormatter(weather.list[0]);
+          //6,14,22,30,38
+          fiveDays = [];
+          for (let i = 6; i < weather.list.length; i += 8) {
+            fiveDays.push(weather.list[i]);
+          }
+          fiveDayFormatter(fiveDays);
         })
         .catch(function (error) {
           alert(error);
@@ -73,5 +143,18 @@ var findCity = function (cityId) {
       alert(error);
     });
 };
+function init() {
+  var storedCity = JSON.parse(localStorage.getItem("found"));
+  if (storedCity === null) {
+    return;
+  }
+  pastCity.innerHTML = "";
+  for (const city of storedCity) {
+    console.log(city);
+    pastCity.innerHTML += `<button class="col-2 btn past cityBtn px-2 ml-2">${city}</button>`;
+  }
+}
 
 searchButton.addEventListener("click", beginSearch);
+
+init();
