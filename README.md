e433704d7a75312be0cdd2b5176305712f9de131
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2022-12-18 12:40:19 -0500

    add readme and comment code

---
+++ README.md
@@ -0,0 +1,24 @@
+# Cloud Nine Weather App
+
+## Description
+
+I built this project in order to give people an easier way to check the weather. I wanted to make a slick and interactive weather app, to provide people with a look at the weather ahead. Instead of constantly searching for realiable weather sites, or checking outside, just look at my website and plan your day accordingly. I learned how to fetch and display API information through this project.
+
+## Installation
+
+N/A
+
+## Usage
+
+To use the app, simply type in the name of a city to look up the weather. Today's weather and the weeks weather will be displayed. If you fail to enter in the name of the city, or the API is down, you will recieve an error.
+
+![image of project](./assets/images/screenshot.png)
+
+## Credits
+
+All images taken from wikimedia commons
+
+banner image by Leviathan1983
+https://commons.wikimedia.org/wiki/User:Leviathan1983
+backround image by Vyacheslav Argenberg
+https://commons.wikimedia.org/wiki/User:Argenberg
--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ var pastCity = document.getElementById("saved");
 var oldCity = document.getElementsByClassName("cityBtn");
 
 console.log(oldCity);
+// formats card for current forcast
 function currentCardFormatter(weather) {
   console.log("Current wether Formatter");
   console.log(weather);
@@ -12,6 +13,7 @@ function currentCardFormatter(weather) {
   //DO I need to clear where I'm appending or do I want duplicates?
   currentCardPlace.innerHTML = "";
   // What element do I create, what will be its content, and where do I append it to?
+  // fetches specific infor from data
   currentCardPlace.innerHTML += `
 <div class="card-body">
   <p class="card-text"> ${weather.dt_txt} </p>
@@ -22,12 +24,13 @@ function currentCardFormatter(weather) {
   
 </div>`;
 }
-
+// formats five day
 function fiveDayFormatter(weatherArr) {
   console.log("Five Days coming up!");
   console.log(weatherArr);
   var fiveDayCard = document.getElementById("five-day");
   fiveDayCard.innerHTML = "";
+  // has to be a quicker way to do this, refactor later if time allows
   fiveDayCard.innerHTML += `
   <div class="col-sm-9 col-lg-3">
   <div class="card">
@@ -87,6 +90,7 @@ function fiveDayFormatter(weatherArr) {
   `;
 }
 
+// searches city name, if no city name returns error
 function beginSearch(event) {
   event.preventDefault();
 
@@ -100,22 +104,27 @@ function beginSearch(event) {
     alert("enter a city name");
   }
 }
+// saves past city
 function saveCity() {
   var searched = cityInput.value.trim();
   var storedCity = JSON.parse(localStorage.getItem("found"));
   if (storedCity === null) {
     storedCity = [];
   }
+  // checks if city name has already been serched, if yes, does not save
   if (storedCity.includes(searched)) {
     return;
   }
+  // if city is greater than 6 replace oldest
   storedCity.push(searched);
   if (storedCity.length > 6) {
     storedCity.shift();
   }
   localStorage.setItem("found", JSON.stringify(storedCity));
+  // displays searched
   init();
 }
+// calls location apu
 var findCity = function (cityId) {
   var url =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -123,14 +132,17 @@ var findCity = function (cityId) {
     "&limit=1&appid=1fe1270c934256178352d8b9b50ce7a4";
   console.log(url);
   fetch(url)
+    // calls for response
     .then(function (response) {
       console.log(response.statusText);
+      // if response is bad throw error
       if (!response.ok) {
         throw new Error(response.statusText);
       } else {
         return response.json();
       }
     })
+    // puts results in weather api
     .then(function (location) {
       console.log(location);
       var lat = location[0].lat;
@@ -153,11 +165,15 @@ var findCity = function (cityId) {
           }
         })
         .then(function (weather) {
+          // is successful call saveCity
           saveCity();
           console.log(weather.list);
+          // inptus current weather
           currentCardFormatter(weather.list[0]);
+          // empty array for five day
           //6,14,22,30,38
           fiveDays = [];
+          // starts at next day counts up by 8
           for (let i = 6; i < weather.list.length; i += 8) {
             fiveDays.push(weather.list[i]);
           }
@@ -171,6 +187,7 @@ var findCity = function (cityId) {
       alert(error);
     });
 };
+// displays saved
 function init() {
   var storedCity = JSON.parse(localStorage.getItem("found"));
   if (storedCity === null) {
@@ -183,6 +200,7 @@ function init() {
   }
 }
 
+// replaces old city info with new
 function reset() {
   currentCardPlace.empty();
   fiveDayCard.empty();
